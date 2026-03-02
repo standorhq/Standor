bf810f4bd7edde4329409e12de470bd2f36eb87d
Ujjwalrohilla12 <rohillaujjwal39@gmail.com>
2026-03-02 22:37:30 +0530

    codebase setup

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import{ENV} from "./lib/env.js"
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({msg:"api is up and running"});
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server running on port:",ENV.PORT);
+});
